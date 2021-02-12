a16fb073220c90341aad6a4fcd1a16655c9fbbcf
solexofficial <solexofficial@yandex.ru>
2021-02-12 22:48:33 +0300

    add aos animation

--- test.js
+++ test.js
@@ -1,13 +1,2 @@
-function multiply(a, b) {
-    result = a * b;
-}
 
-function sum(a, b) {
-    resultSum = +a + +b;
-    return parseInt(resultSum);
-}
-multiply("5", "6");
-sum('5','66');
-console.log(result);
-console.log(resultSum);
-console.log(typeof(resultSum));
+
